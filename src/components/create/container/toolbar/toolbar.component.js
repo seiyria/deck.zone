605bd99ccb6b8527eba7a9ffc06a931c72e47e4b
seiyria <kyle@seiyria.com>
2016-05-13 20:15:07 -0500

    download project as zip

--- src/components/create/container/toolbar/toolbar.component.js
+++ src/components/create/container/toolbar/toolbar.component.js
@@ -5,6 +5,10 @@ import { ProjectComponent } from '../project.component';
 import { ROUTER_DIRECTIVES } from '@angular/router-deprecated';
 import { DROPDOWN_DIRECTIVES } from 'ng2-bootstrap/components/dropdown';
 
+import _ from 'lodash';
+import JSZip from 'jszip';
+import { saveAs } from 'filesaverjs';
+
 @Component({
   selector: 'toolbar',
   directives: [DROPDOWN_DIRECTIVES, ROUTER_DIRECTIVES],
@@ -13,8 +17,19 @@ import { DROPDOWN_DIRECTIVES } from 'ng2-bootstrap/components/dropdown';
 })
 export class ToolbarComponent extends ProjectComponent {
 
-  save() {
-    console.log('save', this.internalProject);
+  _replaceName(filename) { return filename.split('.deck').join('.txt'); }
+
+  download() {
+    const zip = new JSZip();
+
+    _.each(_.values(this.internalProject.scripts), script => {
+      zip.file(this._replaceName(script.name), script.contents);
+    });
+
+    zip.generateAsync({ type: 'blob' })
+      .then((blob) => {
+        saveAs(blob, `${this.internalProject.name}-${Date.now()}.zip`);
+      });
   }
 
 }
